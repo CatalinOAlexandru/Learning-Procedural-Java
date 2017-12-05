6beb806d863b023eb8146f864a24afb3561df71e
Catalin Alexandru <33178694+CatalinOAlexandru@users.noreply.github.com>
2017-12-05 02:18:15 +0000

    sorting methods

--- Testing/Sorting/sorting.java
+++ Testing/Sorting/sorting.java
@@ -9,51 +9,104 @@ public class sorting
   	// Program starting point
     public static void main (String[] param)
     {
-    	sorting(createarray());
+    	//sorting1(createarray()); // simple bubblesort
+    	sorting2(createarray()); // advanced bubblesort
     	System.exit(0);
     }   // END Main
 
-    public static void sorting(int[] numbers);
+    public static void sorting2(int[] numbers)
     {
-    	Print(numbers[0]);
-    	Print(numbers[1]);
-    	Print(numbers[2]);
-    	Print(numbers[3]);
-    	Print(numbers[4]);
-    	Print("");
+    	PrintInt(numbers[0]);
+    	PrintInt(numbers[1]);
+    	PrintInt(numbers[2]);
+    	PrintInt(numbers[3]);
+    	PrintInt(numbers[4]);
 
-    	For(int i=0; i<=numbers.length-2; i++)
-		{
-			If(a[i] > a[i+1])
-			Swap(a, I, i+1)
-		}
+    	boolean sorted=false;
+    	while(!sorted)
+    	{
+    		sorted = true; //array potentially sorted
+    		for(int i=0; i<numbers.length-1; i++)
+    		{
+    			if(numbers[i] > numbers[i+1]) //swap them
+    			{
+    				int tmp = numbers[i+1];
+    				numbers[i+1] = numbers[i];
+    				numbers[i] = tmp;
+    				sorted = false; // array wasn't sorted
+    			}
+    		}
+    	}
+
+    	Print("\nSorted\n");
+		PrintInt(numbers[0]);
+		PrintInt(numbers[1]);
+		PrintInt(numbers[2]);
+		PrintInt(numbers[3]);
+		PrintInt(numbers[4]);
+		Print("\nEND\n");
 
     	return;
     }
 
-    public static int createarray();
+    public static void sorting1(int[] numbers)
+    {
+    	PrintInt(numbers[0]);
+    	PrintInt(numbers[1]);
+    	PrintInt(numbers[2]);
+    	PrintInt(numbers[3]);
+    	PrintInt(numbers[4]);
+
+    	for(int pass=0; pass<=numbers.length-1; pass++)
+		{
+			for(int i=0; i<numbers.length-1-pass; i++)
+			{
+				if(numbers[i]>numbers[i+1])
+				swap(numbers, i ,i+1);
+			}
+		}
+
+		Print("\nSorted\n");
+		PrintInt(numbers[0]);
+		PrintInt(numbers[1]);
+		PrintInt(numbers[2]);
+		PrintInt(numbers[3]);
+		PrintInt(numbers[4]);
+		Print("\nEND\n");
+
+    	return;
+    }
+
+    public static int[] createarray()
     {
     	int[] numbers = new int[5];
     	numbers[0] = 5;
     	numbers[1] = 58;
     	numbers[2] = 4;
-    	numbers[3] = 6;
+    	numbers[3] = 60;
     	numbers[4] = -15;
 
     	return numbers;
     }
 
-    public static void swap(int[]a, int I, int j)
+    public static void swap(int[]a, int i, int j)
 	{
 		int t = a[j];
 		a[j] = a[i];
 		a[i] = t;
 	}
 
-    // a method which will allow me to print messages faster
+    // a method which will allow me to print string messages faster
     public static void Print(String p)
 	{
 		System.out.println(p);
+		return;
+	} // END Print
+
+	// a method which will allow me to print numbers faster
+	public static void PrintInt(int pi)
+	{
+		System.out.println(pi);
 		return;
 	} // END Print
 
